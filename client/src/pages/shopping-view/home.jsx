b6cb7d968bb11a2283f93fa1bc565278b6dc3923
M.M.D. KARUNATHILAKE <manuga.dewhan@gmail.com>
2025-02-12 13:31:46 +0530

    gap adjested

--- client/src/pages/shopping-view/home.jsx
+++ client/src/pages/shopping-view/home.jsx
@@ -134,7 +134,7 @@ function ShoppingHome() {
       <section className="py-12 bg-gray-50">
   <div className="container px-4 mx-auto">
     <h2 className="mb-8 text-3xl font-bold text-center text-purple-700">Inventory Category</h2>
-    <div className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-6">
+    <div className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-6 p-4">
       {categorysWithIcon.map((brandItem) => (
         <Card
           key={brandItem.label} // Add a unique key for each element
